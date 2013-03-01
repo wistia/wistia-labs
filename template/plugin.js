5ca9cf6d77ed7434fa9a1c5e25086448e00b4bce
Max Schnur <mschnur@wistia.com>
2013-03-01 12:05:39 -0500

    Add lab template.

---
+++ template/plugin.js
@@ -0,0 +1,8 @@
+Wistia.plugin("pluginName", function(video, options) {
+
+  // Cool plugin stuff goes here.
+
+  // Return an object with a public interface 
+  // for the plugin, if you want.
+  return {};
+});
