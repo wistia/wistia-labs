d17e301af4c359f3e5c2f71493c55d370d4d2329
Jason Lawrence <jason@wistia.com>
2013-03-04 12:15:39 -0500

    Adding initial working plugin js.

--- logo-over-video/wistia-logo-over-video.js
+++ logo-over-video/wistia-logo-over-video.js
@@ -1,6 +1,38 @@
-Wistia.plugin("pluginName", function(video, options) {
+Wistia.plugin("wistialogoovervideo", function(video, options) {
+  var debug = options.debug || false;
+  function log(){
+    if (debug && (console !== undefined)) { console.log.apply(console, arguments); }
+  };
 
-  // Cool plugin stuff goes here.
+  var logo_url = options.logoUrl || null;
+  var grid_pos = options.pos || 'right_inside';
+  var x_off = options.x_off || '10px';
+  var y_off = options.y_off || '10px';
+  var w_logo = options.w || '100%';
+  var h_logo = options.h || 'auto';
+
+  log("DEBUG");
+  log(video);
+  log(options);
+  log("END DEBUG");
+
+  // Configure the logo image element.
+  var logo_elem = document.createElement("img");
+  logo_elem.style.position = "absolute";
+  logo_elem.style.right = x_off;
+  logo_elem.style.top = y_off;
+  logo_elem.src = logo_url;
+
+  // TODO: Test cross browser scaling!
+  logo_elem.style.width = w_logo;
+  logo_elem.style.height = h_logo;
+
+  // TODO: Check against array of api supported options.
+  if (video.grid[grid_pos] === undefined){
+    log("BAD GRID POSITION!", grid_pos);
+  } else {
+    video.grid[grid_pos].appendChild(logo_elem);
+  }
 
   // Return an object with a public interface 
   // for the plugin, if you want.
