ce6750be5b2f1c4adca4a024b988f77d0670e301
Max Schnur <mschnur@wistia.com>
2013-03-05 17:29:26 -0500

    Initial dim the lights

---
+++ dim-the-lights/ui.js
@@ -0,0 +1,44 @@
+function updateOutput() {
+  var sourceEmbedCode = Wistia.EmbedCode.parse($("#source_embed_code").val());
+  var outputEmbedCode = Wistia.EmbedCode.parse($("#source_embed_code").val());
+
+  if (sourceEmbedCode && sourceEmbedCode.isValid()) {
+
+    // Set custom options on the embed code.
+    // CHANGE ME!!!
+    outputEmbedCode.setOption("myCustomOption", "giraffe");
+
+    // Display the output.
+    $("#output_embed_code").val(outputEmbedCode.toString());
+    outputEmbedCode.previewInElem("preview");
+
+  } else {
+
+    // Show an error if invalid. We can be more specific 
+    // if we expect a certain problem.
+    $("#output_embed_code").val("Please enter a valid Wistia embed code.");
+    $("#preview").html('<div id="placeholder_preview">Your video here</div>');
+  }
+}
+
+
+// Updating is kind of a heavy operation; we don't want to 
+// do it on every single keystroke.
+var updateOutputTimeout;
+function debounceUpdateOutput() {
+  clearTimeout(updateOutputTimeout);
+  updateOutputTimeout = setTimeout(updateOutput, 500);
+}
+
+
+// Assign all DOM bindings on doc-ready in here. We can also 
+// run whatever initialization code we might need.
+window.setupLabInterface = function($) {
+  $(function() {
+    // Update the output whenever a configuration input changes.
+    $("#configure")
+      .on("keyup", "input[type=text], textarea", debounceUpdateOutput)
+      .on("change", "select", debounceUpdateOutput)
+      .on("click", ":radio,:checkbox", debounceUpdateOutput);
+  });
+};
