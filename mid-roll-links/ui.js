8d0cf5fc3118a5a69b54409962bce9263e4f7a43
Jeff Vincent <jeff@wistia.com>
2013-03-05 10:02:08 -0500

    Mid roll links 2nd pass

--- mid-roll-links/ui.js
+++ mid-roll-links/ui.js
@@ -1,54 +1,60 @@
+// we only want to embed once
+var embedded = false;
+
+// if no http on linkHref, let's add it
+function maybeAddHttp(href) {
+  if (href.indexOf("http") == -1) {
+    return "http://" + href;
+  }
+  return href;
+}
+
+// does all the works
 function updateOutput() {
-  var sourceEmbedCode = newEmbedCodeFromPage();
+  var sourceEmbedCode = Wistia.EmbedCode.parse($("#source_embed_code").val());
   var outputEmbedCode = Wistia.EmbedCode.parse($("#source_embed_code").val());
 
-  var hasFullscreen = newEmbed.options().fullscreenButton == null || newEmbed.options().fullscreenButton;
-  var hasMidRoll = Wistia.obj.get(newEmbed.options(), "plugin.midRollLinks.links");
-  var recapNames = $("#postroll_recap").is(":checked");
+  // update the embed code!
+  function updateEmbedCode() {
+    var midRollData = midRollDataFromPage();
+    var hasFullscreen = sourceEmbedCode.options().fullscreenButton == null || sourceEmbedCode.options().fullscreenButton;
+    var hasMidRoll = Wistia.obj.get(sourceEmbedCode.options(), "plugin.midRollLinks");
+    var fullScreenAlert = "This embed code has fullscreen enabled with mid-rolls." + 
+      "Just so you know, the Follow buttons won't show up when fullscreen. " + 
+      "You might want to <a href='#' class='turn_off_fullscreen'>turn off fullscreen</a>."
 
-  if (hasFullscreen && hasMidRoll && !recapNames) {
-    $("#alert").html("This embed code has fullscreen enabled with mid-rolls. Just so you know, the Follow buttons won't show up when fullscreen. You can leave it as it is, <a href='#' class='recap_names_at_end'>recap the names at the end</a>, or <a href='#' class='turn_off_fullscreen'>turn off fullscreen</a>.").show();
-  } else {
-    $("#alert").html("").hide();
+    // uncomment this for production
+    //outputEmbedCode.setOption('plugin.midRollLinks.src', outputEmbedCode.proto() + 
+    //"//" + Wistia.remote.embedHost({ ssl: outputEmbedCode.ssl() }) + "/labs/mid-roll-links/mid-roll-links.js");
+
+    if (midRollData.length > 0) {
+      outputEmbedCode.setOption('plugin.midRollLinks.src', "http://localhost:8000/mid-roll-links/plugin.js");
+      outputEmbedCode.setOption('plugin.midRollLinks.links', midRollData);
+      previewEmbed.plugin.midRollLinks.update({ "links": midRollData });
+    }
+    $("#output_embed_code").val(outputEmbedCode.toString());
+
+    if (hasFullscreen && hasMidRoll) {
+      $("#alert").html(fullScreenAlert).show();
+    } else {
+      $("#alert").html("").hide();
+    }
   }
 
-  // get the mid rolls data
-  function midrollDataFromPage() {
-    var result = [];
-    $(".midrolls .link_and_time_range_combo").each(function() {
-      var $this = $(this);
-      var linkText = $this.find("input[name=link_text]").val();
-      var linkHref = $this.find("input[name=link_href]").val();
-      var start = $this.find("input[name=start]").val();
-      var end = $this.find("input[name=end]").val();
-      if (link_text && start && end) {
-        result.push({
-          linkText: linkText,
-          linkHref: linkHref,
-          start: parseInt(start, 10),
-          end: parseInt(end, 10)
-        });
-      }
-    });
-    return result;
+  function embedMidRollPreview() {
+    $("#output_embed_code").val(outputEmbedCode.toString());
+    outputEmbedCode.previewInElem("preview");
+    embedded = true;
+    updateEmbedCode();
   }
 
   if (sourceEmbedCode && sourceEmbedCode.isValid()) {
-
-    // Set custom options on the embed code.
-    // CHANGE ME!!!
-
-    outputEmbedCode.setOption("myCustomOption", "giraffe");
-    outputEmbedCode.setOption("nested.option", "cantelope");
-    outputEmbedCode.setOption("also.basic-objects", { extraConfig: true });
-    outputEmbedCode.setOption("anotherOption", $("#another_option").val());
-
-    // Display the output.
-    $("#output_embed_code").val(outputEmbedCode.toString());
-    outputEmbedCode.previewInElem("preview");
-
+    if (!embedded) { 
+      embedMidRollPreview(); 
+    } else {
+      updateEmbedCode();
+    }
   } else {
-
     // Show an error if invalid. We can be more specific 
     // if we expect a certain problem.
     $("#output_embed_code").val("Please enter a valid Wistia embed code.");
@@ -56,48 +62,26 @@ function updateOutput() {
   }
 }
 
-
-// update the embed code!
-function newEmbedCodeFromPage() {
-  var postRollOptions = postRollOptionsFromPage();
-  var newEmbed = window.sourceEmbed = Wistia.EmbedCode.parse($("#source_embed_code").val());
-
-  if ($.trim(postRollOptions.linkText)) {
-    newEmbed.setOption('plugin.midRollLinks.postRoll', postRollOptions);
-    newEmbed.removeOption('plugin.postRoll');
-    newEmbed.removeOption('plugin.postRoll-v1');
-  }
-
-  var midRollData = midrollDataFromPage();
-  if (midRollData.length > 0) {
-    newEmbed.setOption('plugin.midRollLinks.links', midRollData);
-    if ($("#postroll_recap").is(":checked")) {
-      var recapDataHash = {};
-      for (var i = 0; i < midRollData.length; i++) {
-        var linkData = Wistia.extend({}, midRollData[i]);
-        delete linkData.start;
-        delete linkData.end;
-        recapDataHash[linkData.linkText] = personData.linkText;
-        recapDataHash[linkData.linkHref] = personData.linkHref;
-      }
-      var recapData = [];
-      for (var key in recapDataHash) {
-        if (recapDataHash.hasOwnProperty(key)) {
-          recapData.push(recapDataHash[key]);
-        }
-      }
-      newEmbed.setOption('plugin.midRollLinks.recap', recapData);
+// get the mid rolls data that has been entered
+function midRollDataFromPage() {
+  var result = [];
+  $(".midrolls .link_and_time_range_combo").not("#link_and_time_range_combo_template").each(function() {
+    var $this = $(this);
+    var linkText = $this.find("input[name=link_text]").val();
+    var linkHref = maybeAddHttp($this.find("input[name=link_href]").val());
+    var start = $this.find("input[name=start]").val();
+    var end = $this.find("input[name=end]").val();
+    if (linkText && linkHref && parseInt(start, 10) && end) {
+      result.push({
+        linkText: linkText,
+        linkHref: linkHref,
+        start: parseInt(start, 10),
+        end: parseInt(end, 10)
+      });
     }
-  }
-
-  if (Wistia.obj.get(newEmbed.options(), 'plugin.midRollLinks')) {
-    newEmbed.setOption('plugin.midRollLinks.src', newEmbed.proto() + "//" + Wistia.remote.embedHost({ ssl: newEmbed.ssl() }) + "/labs/mid-roll-links/mid-roll-links.js");
-  }
-
-  return newEmbed;
+  });
+  return result;
 }
-
-
 
 // Updating is kind of a heavy operation; we don't want to 
 // do it on every single keystroke.
@@ -112,23 +96,24 @@ function addMidRollInput() {
   var $elem = $("#link_and_time_range_combo_template").clone();
   $elem.show().removeAttr("id");
   $(".midrolls .mid_roll_entries").append($elem);
+  return $elem;
+}
+
+function addMidRollData(link_text, link_href, start, end) {
+  $elem = addMidRollInput();
+  $elem.find("input[name=link_text]").focus().val(link_text);
+  $elem.find("input[name=link_href]").focus().val(link_href);
+  $elem.find("input[name=start]").focus().val(start);
+  $elem.find("input[name=end]").focus().val(end);
 }
 
 // add default midRoll
-function addDefaultMidRoll(link_text, link_href, example_time) {
-  addMidRollInput();
-  $(".midrolls").find("input[name=link_text]").example(link_text);
-  $(".midrolls").find("input[name=link_href]").example(link_href);
-  $(".midrolls").find("input[name=start], input[name=end]").example(example_time);
-}
-
-// add midroll - candidate for removal
-function addMidRoll(link_text, link_href, start, end) {
-  var $lastRangeCombo = $(".midrolls .link_and_time_range_combo").not("#link_and_time_range_combo_template").last();
-  $lastRangeCombo.find("input[name=link_text]").focus().val(link_text).keyup().blur();
-  $lastRangeCombo.find("input[name=link_href]").focus().val(link_href).keyup().blur();
-  $lastRangeCombo.find("input[name=start]").focus().val(start).keyup().blur();
-  $lastRangeCombo.find("input[name=end]").focus().val(end).keyup().blur();
+function addDefaultMidRoll(link_text, link_href, start, end) {
+  $elem = addMidRollInput();
+  $elem.find("input[name=link_text]").example(link_text);
+  $elem.find("input[name=link_href]").example(link_href);
+  $elem.find("input[name=start]").example(start);
+  $elem.find("input[name=end]").example(end);
 }
 
 // Check to see if the midroll inputs are currently filled
@@ -158,17 +143,25 @@ function conditionallyAddNewMidRollInput() {
 
 $(".midrolls").on("keyup", "input[type=text]", conditionallyAddNewMidRollInput);
 
+$(document).on("click", ".turn_off_fullscreen", function(event) {
+  event.preventDefault();
+  var source = Wistia.EmbedCode.parse($("#source_embed_code").val());
+  source.setOption("fullscreenButton", false);
+  $("#source_embed_code").val(source.toString()).keyup();
+});
+
 // Assign all DOM bindings on doc-ready in here. We can also 
 // run whatever initialization code we might need.
 window.setupLabInterface = function($) {
   $(function() {
     // Update the output whenever a configuration input changes.
     $("#configure")
-    .on("keyup", "input[type=text], textarea", debounceUpdateOutput)
-    .on("change", "select", debounceUpdateOutput)
-    .on("click", ":radio,:checkbox", debounceUpdateOutput);
+      .on("keyup", "input[type=text], textarea", debounceUpdateOutput)
+      .on("change", "select", debounceUpdateOutput)
+      .on("click", ":radio,:checkbox", debounceUpdateOutput);
 
-  // Add in default midRoll Example
-  addDefaultMidRoll("Check Out Wistia", "http://wistia.com", "?");
+    // Add in default midRoll Example
+    addDefaultMidRoll("Check Out Wistia", "http://wistia.com", "?", "?");
+    addMidRollData("Oooo Check Out Benny", "hesajerk.com", 03, 10);
   });
 };
