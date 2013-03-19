5bd466a435391d90fdb53d80f3def963750f049f
Max Schnur <mschnur@wistia.com>
2013-03-19 15:48:52 -0400

    Better resumable styles.

--- resumable/resumable.js
+++ resumable/resumable.js
@@ -20,20 +20,23 @@ Wistia.plugin("resumable", function(video, options) {
     "  background-color: rgba(48, 48, 48, 0.82);\n" +
     "  box-shadow: rgba(0, 0, 0, 0.9) 0px 0px 218px 30px inset;\n" +
     "  color: rgb(255, 255, 255);\n" +
-    "  display: block;\n" +
+    "  display: inline-block;\n" +
     "  font-family: 'Open Sans', Arial, sans-serif;\n" +
     "  font-size: 19px;\n" +
     "  font-weight: bold;\n" +
     "  height: " + video.videoHeight() + "px;\n" +
     "  left: 0;\n" +
     "  letter-spacing: 1px;\n" +
-    "  line-height: 26.59375px;\n" +
+    "  line-height: 26px;\n" +
     "  margin-bottom: 17px;\n" +
     "  position: absolute;\n" +
     "  text-align: center;\n" +
     "  top: 0;\n" +
     "  white-space: normal;\n" +
     "  width: " + video.videoWidth() + "px;\n" +
+    "}\n" +
+    "#" + uuid + "_content {\n" +
+    "  position: relative;\n" +
     "}\n" +
     "#" + uuid + "_resume_play {\n" +
     "  *display: inline;\n" +
@@ -81,6 +84,30 @@ Wistia.plugin("resumable", function(video, options) {
     "}";
   }
 
+  var styleElem;
+
+  function removeCss() {
+    var par;
+    if (styleElem && (par = styleElem.parentNode)) {
+      par.removeChild(styleElem);
+      styleElem = null;
+    }
+  }
+
+  function addCss() {
+    styleElem = Wistia.util.addInlineCss(document.getElementById(uuid), resumeCss());
+  }
+
+  function refreshCss() {
+    removeCss();
+    addCss();
+  }
+
+  function centerVertically() {
+    var resumeScreen = document.getElementById(uuid + "_content");
+    resumeScreen.style.top = "" + (Math.max(0, video.videoHeight() - Wistia.util.elemHeight(resumeScreen)) / 2) + "px";
+  }
+
   function removeOverlay() {
     var resumeScreen = document.getElementById(uuid);
     var par;
@@ -106,6 +133,7 @@ Wistia.plugin("resumable", function(video, options) {
       var resumeScreen = document.createElement("div");
       resumeScreen.id = uuid;
       resumeScreen.innerHTML = "" +
+      "<div id='" + uuid + "_content'>" +
       "<div>It looks like you've watched<br />part of this video before!</div>" +
       "<a href='#' id='" + uuid + "_resume_play'>" +
       "  <span id='" + uuid + "_resume_play_arrow'>&nbsp;</span>" +
@@ -113,9 +141,11 @@ Wistia.plugin("resumable", function(video, options) {
       "</a><a href='#' id='" + uuid + "_resume_skip'>" +
       "  <span id='" + uuid + "_resume_skip_arrow'>&nbsp;</span>" +
       "  Skip to where you left off" +
-      "</a>";
+      "</a>" +
+      "</div>";
       video.grid.top_inside.appendChild(resumeScreen);
-      Wistia.util.addInlineCss(resumeScreen, resumeCss());
+      refreshCss();
+      centerVertically();
       var resumePlayElem = document.getElementById(uuid + "_resume_play");
       var resumeSkipElem = document.getElementById(uuid + "_resume_skip");
 
