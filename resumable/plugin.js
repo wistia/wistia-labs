8ccd6aa5770c027a0c99ea77b44456f24f2ebb29
Max Schnur <mschnur@wistia.com>
2013-12-10 11:30:26 -0500

    Attempt to fix something weird about resumable on iOS.

--- resumable/plugin.js
+++ resumable/plugin.js
@@ -46,7 +46,7 @@ Wistia.plugin("resumable", function(video, options) {
     "top: 0;\n" +
     "white-space: normal;\n" +
     "width: " + video.videoWidth() + "px;\n" +
-    "z-index: 1;\n" +
+    "z-index: 2;\n" +
     "}\n" +
     "#" + uuid + "_content {\n" +
     "  position: relative;\n" +
