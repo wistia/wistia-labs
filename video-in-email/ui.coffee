5d54b554c147d9f3c3bbc168ca3f88f07c4da0b4
Mary Schmidt <marykateschmidt@gmail.com>
2014-03-11 17:36:54 -0400

    tweaks to video-in-email defaults

--- video-in-email/ui.coffee
+++ video-in-email/ui.coffee
@@ -5,14 +5,7 @@ class VideoInEmail
     @previewEmbedded = false
     @change = false
     @exampleEmbedCode = """
-    <div id="wistia_n9err1v2yk" class="wistia_embed" style="width:640px;height:388px;"><div itemprop="video" itemscope itemtype="http://schema.org/VideoObject"><meta itemprop="name" content="Choosing Background Music - ReelSEO" /><meta itemprop="duration" content="PT4M31S" /><meta itemprop="thumbnailUrl" content="https://embed-ssl.wistia.com/deliveries/e11ab8bcf30ddcef5f2b2a12c4b038613ac4dd84.bin" /><meta itemprop="contentURL" content="https://embed-ssl.wistia.com/deliveries/f33c77c9db099d06d53074d2f31db5ffab62a074.bin" /><meta itemprop="embedURL" content="https://embed-ssl.wistia.com/flash/embed_player_v2.0.swf?2013-10-04&autoPlay=false&banner=false&controlsVisibleOnLoad=false&customColor=87d452&endVideoBehavior=default&fullscreenDisabled=true&hdUrl%5B2pass%5D=true&hdUrl%5Bext%5D=flv&hdUrl%5Bheight%5D=720&hdUrl%5Bsize%5D=89418647&hdUrl%5Btype%5D=hdflv&hdUrl%5Burl%5D=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2F7a18aef53682b02d48ef5caa54e36b35e87f44fd.bin&hdUrl%5Bwidth%5D=1280&mediaDuration=271.0&playButtonVisible=true&showPlayButton=true&showPlaybar=true&showVolume=true&stillUrl=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2Fe11ab8bcf30ddcef5f2b2a12c4b038613ac4dd84.jpg%3Fimage_crop_resized%3D640x360&unbufferedSeek=false&videoUrl=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2Ff33c77c9db099d06d53074d2f31db5ffab62a074.bin" /><meta itemprop="uploadDate" content="2014-02-27T16:15:27Z" /><object id="wistia_n9err1v2yk_seo" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" style="display:block;height:360px;position:relative;width:640px;"><param name="movie" value="https://embed-ssl.wistia.com/flash/embed_player_v2.0.swf?2013-10-04"></param><param name="allowfullscreen" value="true"></param><param name="allowscriptaccess" value="always"></param><param name="bgcolor" value="#000000"></param><param name="wmode" value="opaque"></param><param name="flashvars" value="autoPlay=false&banner=false&controlsVisibleOnLoad=false&customColor=87d452&endVideoBehavior=default&fullscreenDisabled=true&hdUrl%5B2pass%5D=true&hdUrl%5Bext%5D=flv&hdUrl%5Bheight%5D=720&hdUrl%5Bsize%5D=89418647&hdUrl%5Btype%5D=hdflv&hdUrl%5Burl%5D=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2F7a18aef53682b02d48ef5caa54e36b35e87f44fd.bin&hdUrl%5Bwidth%5D=1280&mediaDuration=271.0&playButtonVisible=true&showPlayButton=true&showPlaybar=true&showVolume=true&stillUrl=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2Fe11ab8bcf30ddcef5f2b2a12c4b038613ac4dd84.jpg%3Fimage_crop_resized%3D640x360&unbufferedSeek=false&videoUrl=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2Ff33c77c9db099d06d53074d2f31db5ffab62a074.bin"></param><embed src="https://embed-ssl.wistia.com/flash/embed_player_v2.0.swf?2013-10-04" allowfullscreen="true" allowscriptaccess="always" bgcolor=#000000 flashvars="autoPlay=false&banner=false&controlsVisibleOnLoad=false&customColor=87d452&endVideoBehavior=default&fullscreenDisabled=true&hdUrl%5B2pass%5D=true&hdUrl%5Bext%5D=flv&hdUrl%5Bheight%5D=720&hdUrl%5Bsize%5D=89418647&hdUrl%5Btype%5D=hdflv&hdUrl%5Burl%5D=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2F7a18aef53682b02d48ef5caa54e36b35e87f44fd.bin&hdUrl%5Bwidth%5D=1280&mediaDuration=271.0&playButtonVisible=true&showPlayButton=true&showPlaybar=true&showVolume=true&stillUrl=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2Fe11ab8bcf30ddcef5f2b2a12c4b038613ac4dd84.jpg%3Fimage_crop_resized%3D640x360&unbufferedSeek=false&videoUrl=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2Ff33c77c9db099d06d53074d2f31db5ffab62a074.bin" name="wistia_n9err1v2yk_html" style="display:block;height:100%;position:relative;width:100%;" type="application/x-shockwave-flash" wmode="opaque"></embed></object><noscript itemprop="description">Choosing Background Music - ReelSEO</noscript></div></div>
-      <script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/E-v1.js"></script>
-      <script>
-      wistiaEmbed = Wistia.embed("n9err1v2yk", {
-        videoFoam: true
-      })
-      </script>
-      <script charset="ISO-8859-1" src="//fast.wistia.com/embed/medias/n9err1v2yk/metadata.js"></script>
+    <iframe src="//fast.wistia.net/embed/iframe/7phpe910v3" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="600" height="338"></iframe>
     """
 
     $("#source_embed_code").on "keyup", =>
@@ -58,8 +51,8 @@ class VideoInEmail
 
   setupExample: ->
     $("#source_embed_code").val(@exampleEmbedCode)
-    $("#video_width").val(500)
-    $("#fallback_link").val("http://i.imgur.com/Y9xpLA6.jpg")
+    $("#video_width").val(600)
+    $("#fallback_link").val("https://home.wistia.com/medias/7phpe910v3")
     @previewEmbedded = false
     @change = false
     @debounceUpdates()
@@ -100,18 +93,20 @@ class VideoInEmail
   updateVideoCode: =>
     Wistia.timeout "updateVideoCode", =>
       $('#video_code').val(@outputVideoCode())
-      styles = { height: '112px', width: '585px', 'font-family': 'Courier' }
+      styles = { height: '200px', width: '585px', 'font-family': 'Courier' }
       $('#video_code').css(styles)
     , 100
     Wistia.hashedId = Wistia.EmbedCode.parse($("#source_embed_code").val()).hashedId()
     Wistia.remote.media "#{Wistia.hashedId}", (media) ->
       Wistia.mp4Url = media.assets.iphone.url.replace('.bin', '/video.mp4')
+      Wistia.posterUrl = media.assets.still.url
+      Wistia.playerColor = media.embed_options.playerColor
 
   outputVideoCode: =>
     """
-    <video width="#{@getVideoWidth()}" height="360" poster="#{@getFallbackLink()}" controls="controls">
-      <source src="#{@getVideoMp4()}" type="video/mp4" />
-      <a href="#{@getFallbackLink()}"><img src="#{@getFallbackLink()}" width="640" height="360" /></a>
+    <video width="#{@getVideoWidth()}" poster="#{@getPosterUrl()}" controls="controls">
+      <source src="#{@getVideoMp4()}" type="video/mp4"/>
+      <a href="#{@getFallbackLink()}"><img src="#{@getPosterUrl().replace('.bin', '.jpg')}?image_play_button=true&image_play_button_color=#{@getPlayerColor()}&image_resize=#{@getVideoWidth()}" width="#{@getVideoWidth()}"/></a>
     </video>
     """
 
@@ -122,7 +117,13 @@ class VideoInEmail
     $("#fallback_link").val()
 
   getVideoMp4: ->
-    return  Wistia.mp4Url
+    return Wistia.mp4Url
+
+  getPosterUrl: ->
+    return Wistia.posterUrl
+
+  getPlayerColor: ->
+    return Wistia.playerColor
 
 
 # Assign all DOM bindings on doc-ready in here. We can also
--- video-in-email/ui.js
+++ video-in-email/ui.js
@@ -11,7 +11,7 @@ VideoInEmail = (function() {
     var _this = this;
     this.previewEmbedded = false;
     this.change = false;
-    this.exampleEmbedCode = "<div id=\"wistia_n9err1v2yk\" class=\"wistia_embed\" style=\"width:640px;height:388px;\"><div itemprop=\"video\" itemscope itemtype=\"http://schema.org/VideoObject\"><meta itemprop=\"name\" content=\"Choosing Background Music - ReelSEO\" /><meta itemprop=\"duration\" content=\"PT4M31S\" /><meta itemprop=\"thumbnailUrl\" content=\"https://embed-ssl.wistia.com/deliveries/e11ab8bcf30ddcef5f2b2a12c4b038613ac4dd84.bin\" /><meta itemprop=\"contentURL\" content=\"https://embed-ssl.wistia.com/deliveries/f33c77c9db099d06d53074d2f31db5ffab62a074.bin\" /><meta itemprop=\"embedURL\" content=\"https://embed-ssl.wistia.com/flash/embed_player_v2.0.swf?2013-10-04&autoPlay=false&banner=false&controlsVisibleOnLoad=false&customColor=87d452&endVideoBehavior=default&fullscreenDisabled=true&hdUrl%5B2pass%5D=true&hdUrl%5Bext%5D=flv&hdUrl%5Bheight%5D=720&hdUrl%5Bsize%5D=89418647&hdUrl%5Btype%5D=hdflv&hdUrl%5Burl%5D=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2F7a18aef53682b02d48ef5caa54e36b35e87f44fd.bin&hdUrl%5Bwidth%5D=1280&mediaDuration=271.0&playButtonVisible=true&showPlayButton=true&showPlaybar=true&showVolume=true&stillUrl=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2Fe11ab8bcf30ddcef5f2b2a12c4b038613ac4dd84.jpg%3Fimage_crop_resized%3D640x360&unbufferedSeek=false&videoUrl=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2Ff33c77c9db099d06d53074d2f31db5ffab62a074.bin\" /><meta itemprop=\"uploadDate\" content=\"2014-02-27T16:15:27Z\" /><object id=\"wistia_n9err1v2yk_seo\" classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" style=\"display:block;height:360px;position:relative;width:640px;\"><param name=\"movie\" value=\"https://embed-ssl.wistia.com/flash/embed_player_v2.0.swf?2013-10-04\"></param><param name=\"allowfullscreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><param name=\"bgcolor\" value=\"#000000\"></param><param name=\"wmode\" value=\"opaque\"></param><param name=\"flashvars\" value=\"autoPlay=false&banner=false&controlsVisibleOnLoad=false&customColor=87d452&endVideoBehavior=default&fullscreenDisabled=true&hdUrl%5B2pass%5D=true&hdUrl%5Bext%5D=flv&hdUrl%5Bheight%5D=720&hdUrl%5Bsize%5D=89418647&hdUrl%5Btype%5D=hdflv&hdUrl%5Burl%5D=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2F7a18aef53682b02d48ef5caa54e36b35e87f44fd.bin&hdUrl%5Bwidth%5D=1280&mediaDuration=271.0&playButtonVisible=true&showPlayButton=true&showPlaybar=true&showVolume=true&stillUrl=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2Fe11ab8bcf30ddcef5f2b2a12c4b038613ac4dd84.jpg%3Fimage_crop_resized%3D640x360&unbufferedSeek=false&videoUrl=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2Ff33c77c9db099d06d53074d2f31db5ffab62a074.bin\"></param><embed src=\"https://embed-ssl.wistia.com/flash/embed_player_v2.0.swf?2013-10-04\" allowfullscreen=\"true\" allowscriptaccess=\"always\" bgcolor=#000000 flashvars=\"autoPlay=false&banner=false&controlsVisibleOnLoad=false&customColor=87d452&endVideoBehavior=default&fullscreenDisabled=true&hdUrl%5B2pass%5D=true&hdUrl%5Bext%5D=flv&hdUrl%5Bheight%5D=720&hdUrl%5Bsize%5D=89418647&hdUrl%5Btype%5D=hdflv&hdUrl%5Burl%5D=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2F7a18aef53682b02d48ef5caa54e36b35e87f44fd.bin&hdUrl%5Bwidth%5D=1280&mediaDuration=271.0&playButtonVisible=true&showPlayButton=true&showPlaybar=true&showVolume=true&stillUrl=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2Fe11ab8bcf30ddcef5f2b2a12c4b038613ac4dd84.jpg%3Fimage_crop_resized%3D640x360&unbufferedSeek=false&videoUrl=https%3A%2F%2Fembed-ssl.wistia.com%2Fdeliveries%2Ff33c77c9db099d06d53074d2f31db5ffab62a074.bin\" name=\"wistia_n9err1v2yk_html\" style=\"display:block;height:100%;position:relative;width:100%;\" type=\"application/x-shockwave-flash\" wmode=\"opaque\"></embed></object><noscript itemprop=\"description\">Choosing Background Music - ReelSEO</noscript></div></div>\n  <script charset=\"ISO-8859-1\" src=\"//fast.wistia.com/assets/external/E-v1.js\"></script>\n  <script>\n  wistiaEmbed = Wistia.embed(\"n9err1v2yk\", {\n    videoFoam: true\n  })\n  </script>\n  <script charset=\"ISO-8859-1\" src=\"//fast.wistia.com/embed/medias/n9err1v2yk/metadata.js\"></script>";
+    this.exampleEmbedCode = "<iframe src=\"//fast.wistia.net/embed/iframe/7phpe910v3\" allowtransparency=\"true\" frameborder=\"0\" scrolling=\"no\" class=\"wistia_embed\" name=\"wistia_embed\" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width=\"600\" height=\"338\"></iframe>";
     $("#source_embed_code").on("keyup", function() {
       _this.previewEmbedded = false;
       _this.change = false;
@@ -57,8 +57,8 @@ VideoInEmail = (function() {
 
   VideoInEmail.prototype.setupExample = function() {
     $("#source_embed_code").val(this.exampleEmbedCode);
-    $("#video_width").val(500);
-    $("#fallback_link").val("http://i.imgur.com/Y9xpLA6.jpg");
+    $("#video_width").val(600);
+    $("#fallback_link").val("https://home.wistia.com/medias/7phpe910v3");
     this.previewEmbedded = false;
     this.change = false;
     return this.debounceUpdates();
@@ -112,7 +112,7 @@ VideoInEmail = (function() {
       var styles;
       $('#video_code').val(_this.outputVideoCode());
       styles = {
-        height: '112px',
+        height: '200px',
         width: '585px',
         'font-family': 'Courier'
       };
@@ -120,12 +120,14 @@ VideoInEmail = (function() {
     }, 100);
     Wistia.hashedId = Wistia.EmbedCode.parse($("#source_embed_code").val()).hashedId();
     return Wistia.remote.media("" + Wistia.hashedId, function(media) {
-      return Wistia.mp4Url = media.assets.iphone.url.replace('.bin', '/video.mp4');
+      Wistia.mp4Url = media.assets.iphone.url.replace('.bin', '/video.mp4');
+      Wistia.posterUrl = media.assets.still.url;
+      return Wistia.playerColor = media.embed_options.playerColor;
     });
   };
 
   VideoInEmail.prototype.outputVideoCode = function() {
-    return "<video width=\"" + (this.getVideoWidth()) + "\" height=\"360\" poster=\"" + (this.getFallbackLink()) + "\" controls=\"controls\">\n  <source src=\"" + (this.getVideoMp4()) + "\" type=\"video/mp4\" />\n  <a href=\"" + (this.getFallbackLink()) + "\"><img src=\"" + (this.getFallbackLink()) + "\" width=\"640\" height=\"360\" /></a>\n</video>";
+    return "<video width=\"" + (this.getVideoWidth()) + "\" poster=\"" + (this.getPosterUrl()) + "\" controls=\"controls\">\n  <source src=\"" + (this.getVideoMp4()) + "\" type=\"video/mp4\"/>\n  <a href=\"" + (this.getFallbackLink()) + "\"><img src=\"" + (this.getPosterUrl().replace('.bin', '.jpg')) + "?image_play_button=true&image_play_button_color=" + (this.getPlayerColor()) + "&image_resize=" + (this.getVideoWidth()) + "\" width=\"" + (this.getVideoWidth()) + "\"/></a>\n</video>";
   };
 
   VideoInEmail.prototype.getVideoWidth = function() {
@@ -138,6 +140,14 @@ VideoInEmail = (function() {
 
   VideoInEmail.prototype.getVideoMp4 = function() {
     return Wistia.mp4Url;
+  };
+
+  VideoInEmail.prototype.getPosterUrl = function() {
+    return Wistia.posterUrl;
+  };
+
+  VideoInEmail.prototype.getPlayerColor = function() {
+    return Wistia.playerColor;
   };
 
   return VideoInEmail;
